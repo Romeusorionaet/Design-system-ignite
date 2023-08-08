42b53eb015eb137498396a8395f5811a3999e840
romeusorionaet <romeusoares14569@gmail.com>
2023-08-08 18:53:02 -0300

    react folder

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Box = styled('div', {
+  padding: '$4',
+  borderRadius: '$md',
+  border: '2px solid $gray900',
+  color: '$gray100',
+})
+
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
+
+Box.displayName = 'Box'
